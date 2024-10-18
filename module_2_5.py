fbfd488a9dfe5a0184f1ee2295b5a792196f6b4b
Kurban2132 <k.kokanov@yandex.ru>
2024-10-18 21:17:14 +0300

    Add files via upload

--- module_2_5.py
+++ module_2_5.py
@@ -1,11 +1,12 @@
 def get_matrix(n, m, value):
     matrix = []
-    for row in range(n):
-        A_list_ = []
-        for colurn in range(m):
-            A_list_.append(value)
-        matrix.append(A_list_)
-        return matrix
+    for row in range(1, n + 1):
+        list_1 = []
+        matrix.append(list_1)
+        for colurn in range(1, m + 1):
+            list_1.append(value)
+            print(matrix)
+
 
 result1 = get_matrix(2, 2, 10) # result1 - исходный код (n)
 result2 = get_matrix(3, 5, 42)  # result2 - исходный код (m)
@@ -13,7 +14,6 @@ result3 = get_matrix(4, 2, 13)  # result3 - исходный код (value)
 print(result1)
 print(result2)
 print(result3)
-
 
 
 
